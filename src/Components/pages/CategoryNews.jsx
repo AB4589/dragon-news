f37dd46d03522ed796f1b04404a45a2deb332c6e
AB4589 <tarekrahat4@gmail.com>
2026-06-04 16:32:22 +0600

    News Filter Added

--- src/Components/pages/CategoryNews.jsx
+++ src/Components/pages/CategoryNews.jsx
@@ -1,11 +1,27 @@
-import {useParams} from "react-router";
+import { useEffect, useState } from "react";
+import {useLoaderData, useParams} from "react-router";
 
 const CategoryNews = () => {
     const { id } = useParams();
-    console.log(id);
+    const data = useLoaderData();
+    const [categoryNews, setCategoryNews] = useState([]);
+    useEffect(()=> {
+            if(id == "00"){
+                setCategoryNews(data);
+                return;
+            }else if(id=='1'){
+                const filteredNews = data.filter((news)=>news.is_todays_pick==true)
+                setCategoryNews(filteredNews);
+                return;
+            }
+            const filteredNews = data.filter(news=>news.category.id == id);
+            console.log(filteredNews)
+            setCategoryNews(filteredNews);
+        }, [data, id]
+    ) 
     return (
         <div>
-            Catergory News - {id}
+            Total - {categoryNews.length} News Found
         </div>
     );
 };
--- src/layouts/SideLayout/QZone.jsx
+++ src/layouts/SideLayout/QZone.jsx
@@ -1,9 +1,75 @@
 const QZone = () => {
     return (
-        <div className="bg-[#F3F3F3] rounded-xl p-4 ">
+        <>
+        <div className="bg-[#F3F3F3] p-4 space-y-10 mt-5">
             <h2 className="text-xl text-[#403F3F]">Q-Zone</h2>
-          
+            
+  <div class="relative w-64">
+ 
+    <div class="absolute -bottom-2 left-2 right-2 h-full bg-white rounded-2xl shadow-md rotate-2"></div>
+    <div class="absolute -bottom-1 left-1 right-1 h-full bg-white rounded-2xl shadow-sm -rotate-1"></div>
+ 
+    <div class="relative bg-white rounded-2xl shadow-xl overflow-hidden pb-5">
+ 
+      <div class="overflow-hidden rounded-xl m-3 mb-0">
+        <img
+          src="https://images.unsplash.com/photo-1560089000-7433a4ebbd64?w=600&q=80"
+          alt="Child swimming with kickboard"
+          class="w-full h-52 object-cover rounded-xl"
+        />
+      </div>
+ 
+      <div class="text-center mt-4 px-4">
+        <span class="text-xl font-extrabold text-gray-800 tracking-wide">Swimming</span>
+      </div>
+ 
+    </div>
+  </div>
+   <div class="relative w-64">
+ 
+    <div class="absolute -bottom-2 left-2 right-2 h-full bg-white rounded-2xl shadow-md rotate-2"></div>
+    <div class="absolute -bottom-1 left-1 right-1 h-full bg-white rounded-2xl shadow-sm -rotate-1"></div>
+ 
+    <div class="relative bg-white rounded-2xl shadow-xl overflow-hidden pb-5">
+ 
+      <div class="overflow-hidden rounded-xl m-3 mb-0">
+        <img
+          src="https://images.unsplash.com/photo-1560089000-7433a4ebbd64?w=600&q=80"
+          alt="Child swimming with kickboard"
+          class="w-full h-52 object-cover rounded-xl"
+        />
+      </div>
+ 
+      <div class="text-center mt-4 px-4">
+        <span class="text-xl font-extrabold text-gray-800 tracking-wide">Swimming</span>
+      </div>
+ 
+    </div>
+  </div>
+   <div class="relative w-64">
+ 
+    <div class="absolute -bottom-2 left-2 right-2 h-full bg-white rounded-2xl shadow-md rotate-2"></div>
+    <div class="absolute -bottom-1 left-1 right-1 h-full bg-white rounded-2xl shadow-sm -rotate-1"></div>
+ 
+    <div class="relative bg-white rounded-2xl shadow-xl overflow-hidden pb-5">
+ 
+      <div class="overflow-hidden rounded-xl m-3 mb-0">
+        <img
+          src="https://images.unsplash.com/photo-1560089000-7433a4ebbd64?w=600&q=80"
+          alt="Child swimming with kickboard"
+          class="w-full h-52 object-cover rounded-xl"
+        />
+      </div>
+ 
+      <div class="text-center mt-4 px-4">
+        <span class="text-xl font-extrabold text-gray-800 tracking-wide">Swimming</span>
+      </div>
+ 
+    </div>
+  </div>
         </div>
+
+        </>
     );
 };
 
